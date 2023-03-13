1463a469c5d7dec9fc3c915e1a51bf071ec87d0a
SolangeZ <93062167+SolangeZ@users.noreply.github.com>
2023-03-12 21:13:40 -0300

    Agregando Card

--- parcial-front3/src/Card.jsx
+++ parcial-front3/src/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = (props) => {
   return (
-                <div>
+                <div id = "card-container">
                     <p>Hola {props.name},</p>
                     <p>Tu personaje favorito es:</p>
                     <p>{props.personaje}</p>
